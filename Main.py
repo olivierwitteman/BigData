229aed70af44219ea4cd1888d9ca86cd80540297
Olivier Witteman <olivier@2001.net>
2020-04-01 11:03:13 +0200

    Changed to random forest for dataset compatibility

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
-from sklearn.cluster import KMeans
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import make_regression
 import numpy as np
 import time
 import scipy.stats as sps
@@ -7,12 +8,15 @@ from scitime import Estimator
 
 # example for kmeans clustering
 estimator = Estimator(meta_algo='RF', verbose=3)
-km = KMeans()
+rfr = RandomForestRegressor()
 
 # generating inputs for this example
-X = np.random.rand(100000, 10)
+# X = np.random.rand(100000, 10)
+
+X, y = make_regression(n_samples=int(5e4), n_features=8, random_state=0)
+
 # run the estimation
-estimation, lower_bound, upper_bound = estimator.time(km, X)
+estimation, lower_bound, upper_bound = estimator.time(rfr, X, y=y)
 
 n = 30
 tlst = []
@@ -20,12 +24,12 @@ tlst = []
 
 for _ in range(n):
     t0 = time.time()
-    km.fit(X)
+    rfr.fit(X, y)
     tlst.append(time.time()-t0)
 
 print('Average time: {!s}s'.format(round(np.average(tlst))))
 
 print(sps.describe(tlst))
-y = km.predict(X)
+y = rfr.predict(X)
 
-print(y)
+print(y.shape)
--- time_complexity.py
+++ time_complexity.py
@@ -1,18 +1,20 @@
-def big_o(n_time, algo='NN', d=1, k=1, nodes=None, t=None):
+import numpy as np
+
+
+def big_o(n_time, algo='NN', nodes=None, t=None):
     """
     Calculates the expected computation effort compared to n_time
 
     :param n_time: Calculation time for baseline n
     :param algo: algorithm to calculate computation effort for
-    :param d: algorithm parameter
-    :param k: algorithm parameter
-    :param nodes: list of nodes (ie. [a, b, c, d] for a 4 layer network)
+    :param nodes: list of node sizes (ie. [a, b, c, d] for a 4 layer network)
     :param t: training examples
     :return: Calculation time extrapolated to parameters that will be used
     """
 
     if algo == 'KMeans':
-        return n_time ** (d * k + 1)
+        # return n_time ** (d * k + 1)
+        return None
     elif algo == 'SVC':
         return None
     elif algo == 'RandomForestClassifier':
@@ -27,23 +29,24 @@ def big_o(n_time, algo='NN', d=1, k=1, nodes=None, t=None):
         return n_time * t * nodecomplexity
 
 
-def big_o_inv(time, algo='KMeans', d=1, k=1):
+def big_o_inv(time, algo='RandomForestRegressor', n=1, t=1):
     """
     Calculates baseline n from a sample training with parameters used
 
     :param time: Actual calculation time using given parameters
     :param algo: algorithm used
-    :param d: algorithm parameter
-    :param k: algorithm parameter
+    :param n: algorithm parameter sample size
+    :param t: algorithm parameter trees
     :return: Baseline n calculation time
     """
     if algo == 'KMeans':
-        return time ** -(d * k + 1)
+        # return time ** -(d * k + 1)
+        return None
     elif algo == 'SVC':
         return None
     elif algo == 'RandomForestClassifier':
         return None
     elif algo == 'RandomForestRegressor':
-        return None
+        return np.sqrt(time / (n * t))
     else:
         return None
